d0469b8718fba7ff50052d8fb84405ec3a67811f
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-04-07 10:22:51 -0300

    feat: create grid

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
-import { Flex } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
 
 export default function Dashboard() {
   return (
@@ -8,6 +8,18 @@ export default function Dashboard() {
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+          <Box p="8" bg="gray.800" borderRadius={8}>
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
